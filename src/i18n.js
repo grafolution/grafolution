18222fd1ea6fd94002a676266af624f33e607a9c
grafolution-me <grafservices@gmx.at>
2023-11-06 00:02:26 +0100

    translations

--- src/i18n.js
+++ src/i18n.js
@@ -4,19 +4,26 @@ import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-    .use(HttpApi)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: "en", 
-        debug: true,
-        detection: {
-            order: ['queryString', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
-            caches: ['localStorage', 'cookie']
-        },
-        interpolation: {
-            escapeValue: false
-        }
-    });
+  .use(HttpApi) // verwendet das i18next-http-backend
+  .use(LanguageDetector) // erkennt die Sprache des Benutzers
+  .use(initReactI18next) // initialisiert react-i18next
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    detection: {
+      order: ['queryString', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage', 'cookie']
+    },
+    backend: {
+      // der Pfad zu Ihren Übersetzungsdateien
+      loadPath: '/grafolution/locales/{{lng}}/{{ns}}.json',
+    },
+    interpolation: {
+      escapeValue: false // not needed for react as it escapes by default
+    },
+    react: {
+      useSuspense: false // falls Sie Suspense nicht verwenden
+    },
+  });
 
 export default i18n;
