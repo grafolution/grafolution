22db65aecb78d8c1189a0ba0e5b7230e7c8470a4
grafolution-me <grafservices@gmx.at>
2023-11-06 00:21:34 +0100

    updates in main pages (sections

--- src/components/section.js
+++ src/components/section.js
@@ -25,39 +25,40 @@ const Section = () => {
 
 
 
-            <section id="section1" className={styles.section}>
-                <h2>Bildungsreise</h2>
+        <section id="section1" className={styles.section}>
+            <h2>{t('educationJourney.title')}</h2>
 
-                <VerticalTimeline>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="Jan. 2020–Juli 2023"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    >
-                        <h3>Fachhochschule Oberösterreich</h3>
-                        <p>Bachelor's degree in Computer Software Engineering</p>
-                        <p>Schwerpunkte: Algorithmen, Computernetzwerke und Programmierframeworks.</p>
-                    </VerticalTimelineElement>
+            <VerticalTimeline>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={t('educationJourney.timeline1.date')}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            >
+                <h3>{t('educationJourney.timeline1.title')}</h3>
+                <p>{t('educationJourney.timeline1.degree')}</p>
+                <p>{t('educationJourney.timeline1.description')}</p>
+            </VerticalTimelineElement>
 
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="2014–2019"
-                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                    >
-                        <h3>HTBLuVA St. Pölten</h3>
-                        <p>Matura in Informatik</p>
-                    </VerticalTimelineElement>
-                </VerticalTimeline>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date={t('educationJourney.timeline2.date')}
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            >
+                <h3>{t('educationJourney.timeline2.title')}</h3>
+                <p>{t('educationJourney.timeline2.degree')}</p>
+            </VerticalTimelineElement>
+            </VerticalTimeline>
 
-                <h2>Fähigkeitenbaum</h2>
-                <div className={styles.tree}>
-                    <div className={styles.node}>Grundfähigkeiten</div>
-                    <div className={styles.branch}>
-                        <div className={styles.node}>Java</div>
-                        <div className={styles.node}>.NET</div>
-                    </div>
-                </div>
-            </section>
+            <h2>{t('skillsTree.title')}</h2>
+            <div className={styles.tree}>
+            <div className={styles.node}>{t('skillsTree.basicSkills')}</div>
+            <div className={styles.branch}>
+                <div className={styles.node}>{t('skillsTree.skill1')}</div>
+                <div className={styles.node}>{t('skillsTree.skill2')}</div>
+            </div>
+            </div>
+        </section>
+
 
             <section id="section3" className={styles.section}>
                 <div>
